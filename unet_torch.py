ad805646b8bd203555ac6e1c296ddc5bd0b837f6
wjy <1339807619@qq.com>
2020-02-15 08:33:16 +0800

    更新torch文件

--- unet_torch.py
+++ unet_torch.py
@@ -7,17 +7,24 @@
 # 
 ################################################################################################
 
-
-
 import torch.nn as nn
 import torch
 from torch import autograd
+from torchvision import transforms as tfs
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 import cv2
+import math
+
+im_aug1 = tfs.Compose([
+    tfs.Resize(200),
+    tfs.RandomHorizontalFlip(),
+    tfs.RandomCrop(128),
+    tfs.ToTensor()
+])
 
 #把常用的2个卷积操作简单封装下
 class DoubleConv(nn.Module):
@@ -106,6 +113,12 @@ class trainData():
         self.image_data = image_data
         return image_data
     
+    def decode(self, image, min=0.0, max=255.0):
+        image[image < min] = min
+        image[image > max] = max
+        image = image / max
+        return image
+
     def get_label_file(self):
         ''' 标签数据为 512 * 512, 为0/1 '''
         file_dir = self.label_dir
@@ -142,22 +155,96 @@ class trainData():
                 figs[i][j].imshow(img_item)
         plt.show()
     
-    def get_train_data(self):
+    def get_train_data(self,number=50,batch_size=5,channel=1):
         ''' 先试试使用未经处理的图像数据 '''
         self.get_image_file()
         self.get_label_file()
-        return self.image_data, self.label_data
+
+        self.batch_size = 5
+        self.channel = channel
+        
+        # 此时数据范围较大, 对某范围内数值进行压缩处理, 其余直接取极值
+        l_temp = []
+        for i in self.image_data:
+            i = self.decode(i, 0.0, 300.0)
+            l_temp.append(i)
+        self.image_data = l_temp
+
+        train_data = []
+        train_label = []
+        while len(train_data) < number: 
+            l1 = []
+            l2 = []
+            for i in range(batch_size):
+                ind = random.randint(0, len(self.image_data) - channel)
+                temp = self.image_data[ind]
+                # temp = temp[:,:,np.newaxis,np.newaxis]
+                temp = temp[np.newaxis,np.newaxis,:,:]
+                for i in range(ind+1, ind + channel):
+                    torch.cat([temp, self.image_data[i].unsqueeze(0)], dim=1)
+                l1.append(temp)
+
+                temp = self.label_data[ind]
+                # temp = temp[:,:,np.newaxis,np.newaxis]
+                temp = temp[np.newaxis,np.newaxis,:,:]
+                for i in range(ind+1, ind + channel):
+                    torch.cat([temp, self.label_data[i].unsqueeze(0)], dim=1)
+                l2.append(temp)
+            
+            t1 = l1[0]
+            t2 = l2[0]
+            for i in range(1,batch_size):
+                # torch.cat([t1, l1[i]], dim=3)
+                # torch.cat([t2, l2[i]], dim=3)
+                t1 = np.concatenate((t1, l1[i]), axis=0)
+                t2 = np.concatenate((t2, l2[i]), axis=0)
+            # 将numpy转化为torch.tensor
+            t1 = torch.from_numpy(t1)
+            t1 = torch.tensor(t1, dtype=torch.float32)
+            t2 = torch.from_numpy(t2)
+            
+            train_data.append(t1)
+            train_label.append(t2)
+        
+        return train_data,train_label
+        # return self.image_data, self.label_data
+
+def train_model(net, xx, yy, EPOCH=100, learning_rate=0.05):
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+    loss_func = torch.nn.CrossEntropyLoss()
+    
+    accuracy = 0
+    for epoch in range(EPOCH):
+        step = 0
+
+        for step, b_x in enumerate(xx):
+            output = net(b_x)  # cnn output
+
+            loss = loss_func(output, yy[step])  # cross entropy loss
+            # clear gradients for this training step
+            optimizer.zero_grad()
+            loss.backward()  # backpropagation, compute gradients
+            optimizer.step()  # apply gradients
+
+            if step % 1 == 0:
+                pred = net(b_x)
+                # print("\r" + 'Epoch: ' + str(epoch) + ' step: ' + str(step) + '[' +">>>" * int(step / 10) + ']',end=' ')
+                print('Epoch:{} step:{}'.format(epoch, step),'loss: %.4f' % loss.data.numpy())
+                # print('loss: %.4f' % loss.data.numpy(), '| accuracy: %.4f' % accuracy, end=' ')
+                # print('loss: %.4f' % loss.data.numpy(), end=' ')
+        # print('')
 
 if __name__ == "__main__":
     # 处理源图像与标记数据
     trainDataLoader = trainData()
     trainDataLoader.test_show()
-    
-    x,y = trainDataLoader.get_train_data()
+    x, y = trainDataLoader.get_train_data(number=50,batch_size=5,channel=1)
     # 运行unet
-    # model = Unet()
+    model = Unet(in_ch=1,out_ch=1)
+    print(model)
     
+    train_model(model,x,y,100,0.05)
     # 测试准确率
-
+    print()
 
 
